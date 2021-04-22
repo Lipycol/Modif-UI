cfe8695aa2ab94b3035ac263408a34d01fa233f1
Lipycol <59168092+Lipycol@users.noreply.github.com>
2021-04-22 21:35:25 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from './components';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button text={'helloworld'}></Button>
+      </div>
+    );
+  }
+}
+
+export default App;
