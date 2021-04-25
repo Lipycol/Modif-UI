c03d544fffa5fffea423e5a8cfc1ec63dd98aa43
Lipycol <59168092+Lipycol@users.noreply.github.com>
2021-04-25 16:20:10 +0800

    文档创建

---
+++ doczrc.js
@@ -0,0 +1,9 @@
+import { css } from 'docz-plugin-css'
+
+export const title = 'Modif-UI';
+export const description = 'A Design UI library for React';
+export const plugins = [
+  css({
+    preprocessor: 'postcss'
+  })
+];
